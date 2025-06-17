edf879492ffd4d229b0ea7c4420a46e15189a0b7
coolcoderdivyaansh <divyaanshpardeshi31@gmail.com>
2025-06-17 12:21:53 +0530

    acp java 1

---
+++ LESSON 1/acp 1.js
@@ -0,0 +1,15 @@
+var name = "DIVYAANSH PARDESHI";
+var age = 11;
+var skills = ["HTML", "CSS", "bootstrap"];
+var schoolName = "Air Force Golden Jubilee Institute";
+var phoneNumber = "6351415056";
+var address = "New Delhi, India";
+var email = "divyaanshpardeshi31@gmail.com";
+console.log("HI MY NAME IS :", name)
+console.log(" MY AGE IS :", age)
+console.log(" MY SKILLS ARE :", skills)
+console.log("My School Name is : ",schoolName)
+console.log("My Phone Number is : ", phoneNumber)
+console.log("My Address is : ", address)
+console.log("My Email is : ", email)
+
