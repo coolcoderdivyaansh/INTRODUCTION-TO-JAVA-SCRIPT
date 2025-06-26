74d2768da7b973b77fa5832721dc67252ff2b94e
coolcoderdivyaansh <divyaanshpardeshi31@gmail.com>
2025-06-26 19:31:08 +0530

    cw java 3

---
+++ LESSON 3/day.js
@@ -0,0 +1,28 @@
+const day="Tuesday";
+
+switch(day) {
+    case "Monday":
+        console.log(" start of the week!");
+        break
+        case "Tuesday":
+            console.log("Its Tuesday Keep Going!");
+            break
+        case "Wednesday":
+            console.log("Its Wednesday, Halfway there!");
+            break
+            case "Thursday":
+                console.log("Its Thursday, Almost there!");
+                break
+                case "Friday":
+                    console.log(" Weekend is near!");
+                    break
+                case "Saturday":
+                    console.log("Its Saturday, Enjoy!");
+                    break
+                case "Sunday":
+                    console.log("Its Sunday, Rest well!");
+                    break
+                    default:
+                        console.log("Its A Regular Day!");
+                        break
+}
---
+++ LESSON 3/loop.js
@@ -0,0 +1,9 @@
+//for (init ; cond ; incre)
+for (let i = 0; i <=1000; i++) {
+    console.log("value of 1 is:", i);
+}
+let i = 0;
+while ( i<=1000) {
+    console.log("inside while loop is:", i);
+    i++;
+}
